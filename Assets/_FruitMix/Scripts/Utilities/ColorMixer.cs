ac0963e5df51ee2f200fac17d6f2946741b9e7ac
Darrior <oleg.grodas@gmail.com>
2022-08-09 18:30:03 +0300

    add shadows, volume, color correction

--- Assets/_FruitMix/Scripts/Utilities/ColorMixer.cs
+++ Assets/_FruitMix/Scripts/Utilities/ColorMixer.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -12,14 +11,6 @@ namespace _FruitMix.Scripts.Utilities
             Color newColor = new Color(0, 0, 0, 1);
             newColor = colors.Aggregate(newColor, (current, color) => current + color);
             return newColor / colors.Count;
-        }
-
-        public static float CompareColor(Color color1, Color color2)
-        {
-            var colorR = (color1[0] / color2[0]);
-            var colorG = (color1[1] / color2[1]);
-            var colorB = (color1[2] / color2[2]);
-            return (colorR + colorG + colorB) / 3;
         }
     }
 }
